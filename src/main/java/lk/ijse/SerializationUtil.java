82cb79a5e6ed28bb31f56ab30ae533d2619959b7
bandara <chathubandara500@gmail.com>
2024-07-18 16:59:15 +0530

    SerializationUtil class

---
+++ src/main/java/lk/ijse/SerializationUtil.java
@@ -0,0 +1,20 @@
+package lk.ijse;
+
+import java.io.*;
+
+public class SerializationUtil {
+    public static byte[] serialize(Object obj) throws IOException {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream out = new ObjectOutputStream(bos)) {
+            out.writeObject(obj);
+            return bos.toByteArray();
+        }
+    }
+
+    public static Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+             ObjectInputStream in = new ObjectInputStream(bis)) {
+            return in.readObject();
+        }
+    }
+}
